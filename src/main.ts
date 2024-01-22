d2db459326c108c366bc77355320a17964a8bcd8
David Podhola <david.podhola@headout.com>
2024-01-22 17:47:24 +0100

    we have styles!

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import './app.css'
-import App from './App.svelte'
-import {setupLocales} from "./i18n";
-import {authenticate} from "./keycloak";
-import {apolloClientGenerator} from "./lib/apollo-client";
+import "./app.pcss";
+import App from "./App.svelte";
+import { setupLocales } from "./i18n";
+import { authenticate } from "./keycloak";
+import { apolloClientGenerator } from "./lib/apollo-client";
 
 setupLocales();
 // @ts-ignore
-authenticate(
-    (token) => apolloClientGenerator(token)
-);
+authenticate((token) => apolloClientGenerator(token));
 
 console.log({
-  someKey: import.meta.env.VITE_SOME_KEY
-})
+  someKey: import.meta.env.VITE_SOME_KEY,
+});
 
 const app = new App({
   // @ts-ignore
-  target: document.getElementById('app'),
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
