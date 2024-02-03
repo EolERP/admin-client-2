63de29b2095444a53f0d0179b13c39537106502b
David Podhola <david.podhola@headout.com>
2024-02-03 18:46:05 +0100

    new admin2

--- build_deploy_dokku_web.sh
+++ build_deploy_dokku_web.sh
@@ -5,7 +5,7 @@ rm -rf build
 VITE_API_BASE_URL=https://erpjs-api.prod.eolerp.eu/graphql \
 VITE_KEYCLOAK_BASE_URL=https://keycloak.prod.eolerp.eu/auth \
 VITE_KEYCLOAK_REALM=erpjs \
-VITE_URL=https://admin.prod.eolerp.eu/ \
+VITE_URL=https://admin2.prod.eolerp.eu/ \
  npm run build
 
 cd dist
