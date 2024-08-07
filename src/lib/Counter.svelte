d12b87fa2ce21b159466944f7a1e042fd60632ba
David Podhola <david.podhola@eolerp.eu>
2024-08-07 15:38:48 +0200

    remove custs

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-  import {_} from "svelte-i18n";
   import {SalesInvoices} from "./graphql/generated";
   $: invoices = SalesInvoices({})
-
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
 </script>
-
-<button on:click={increment}>
-  {$_('temp.count')} {count}
-</button>
 
 {#if !$invoices}
   <p>.. loading</p>
